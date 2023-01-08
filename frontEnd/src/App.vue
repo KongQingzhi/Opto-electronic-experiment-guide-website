f19042c317dd48e06b026f5b351d45e8bee44bac
kongqingzhi <1428390162@qq.com>
2023-01-08 12:36:29 +0800

    add
    add

--- frontEnd/src/App.vue
+++ frontEnd/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-
+    <div class="App"></div>
 </template>
 
 <script setup lang="ts">
@@ -9,5 +9,44 @@
 
 
 <style lang="scss" scoped>
+.App {
+    height: 20rem;
+    background-color: blueviolet;
+    margin: 0 auto;
+}
+@media only screen and (min-width: 1200px) {
+    .App {
+        width: 75rem;
+    }
+}
 
+@media only screen and (max-width: 1200px) {
+    .App {
+        width: 62rem;
+    }
+}
+
+@media only screen and (max-width: 992px) {
+    .App {
+        width: 50rem;
+    }
+}
+
+@media only screen and (max-width: 768px) {
+    .App {
+        width: 37rem;
+    }
+}
+
+@media only screen and (max-width: 520px) {
+    .App {
+        width: 30rem;
+    }
+}
+
+@media only screen and (max-width: 420px) {
+    .App {
+        width: 100%;
+    }
+}
 </style>
--- frontEnd/src/router/index.ts
+++ frontEnd/src/router/index.ts
@@ -1,7 +1,7 @@
 //1.引入 createWebHashHistory为Hash模式, createWebHistory为history模式
 import { createRouter, createWebHashHistory } from 'vue-router';
 // import Home from './Home';
-// import Show from './Show';
+import ShowVue from '../views/show/Show.vue';
 // import User from './User';
 // import Home from './Home';
 //2.创建路由表
@@ -11,6 +11,11 @@ const routes = [
         name: 'root',
         redirect: "/show",
     },
+    {
+        path: '/show',
+        name: 'show',
+        component:ShowVue
+    }
     // Show,
     // User,
     // Home
