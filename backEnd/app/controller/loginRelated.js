3efd2b3d240267ae60594f8540441a96c1ffe093
KongQingzhi <1428390162@qq.com>
2023-02-23 16:04:58 +0800

    feat:login API

--- backEnd/app/controller/loginRelated.js
+++ backEnd/app/controller/loginRelated.js
@@ -1,11 +1,26 @@
-import { getParams } from '../utils/getParams';
 const { Controller } = require('egg');
 class LoginRelated extends Controller {
+    // async getParams() {
+    //     const { ctx } = this;
+    //     if (ctx.request.method === "GET") {
+    //         return ctx.query
+    //     } else if (ctx.request.method === "POST") {
+    //         return ctx.request.body;
+    //     }
+    // }
+
+    // get请求获取参数
+    // const query = ctx.query;
+    // post请求获取参数
+    // const query = ctx.request.body;
     async login() {
         const { ctx } = this;
-        const query = getParams()
-        console.log(query);
-        ctx.body = query
+        const query = ctx.query;
+        const res = await ctx.service.loginRelated.login(query)
+        ctx.body = {
+            state: 200,
+            data: res
+        };
     }
 }
 module.exports = LoginRelated;
--- backEnd/app/service/loginRelated.js
+++ backEnd/app/service/loginRelated.js
@@ -1,9 +1,17 @@
 const { Service } = require('egg');
 class LoginRelated extends Service {
-    async login(UserNo, UserPassword, Table) {
-        
-        const sql = `select * from ${Table}`
-
+    async login(param) {
+        const { app } = this;
+        const { UserNo, UserPassword, Role } = param;
+        let Table = ''
+        if (Role) {
+            Table = 'students'
+        } else {
+            Table = 'teachers'
+        }
+        const sql = `select * from ${Table} where s_no = ${UserNo} and s_password = ${UserPassword}`
+        const res = await app.mysql.query(sql);
+        return res
     }
 }
 module.exports = LoginRelated;
