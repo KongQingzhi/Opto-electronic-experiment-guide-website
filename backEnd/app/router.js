a0a69cdf925442c355899e3dc0c5f28ac4f62074
KongQingzhi <1428390162@qq.com>
2023-02-24 11:46:26 +0800

    feat:新增登陆相关的接口

--- backEnd/app/router.js
+++ backEnd/app/router.js
@@ -5,5 +5,12 @@
  */
 module.exports = app => {
   const { router, controller } = app;
+  // 登录
   router.get('/api/login', controller.loginRelated.login);
+  // 注册
+  router.post('/api/register', controller.loginRelated.register);
+  // 验证
+  router.post('/api/verify', controller.loginRelated.verify);
+  // 找回密码
+  router.post('/api/retrievePassword', controller.loginRelated.retrievePassword)
 };
