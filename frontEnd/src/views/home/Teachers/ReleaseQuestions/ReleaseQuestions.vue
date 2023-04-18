208b294526cb7304a890164222d0aaf6665dd87e
kongqingzhi <1428390162@qq.com>
2023-04-19 00:02:22 +0800

    feat:新增发布实验

--- frontEnd/src/views/home/Teachers/ReleaseQuestions/ReleaseQuestions.vue
+++ frontEnd/src/views/home/Teachers/ReleaseQuestions/ReleaseQuestions.vue
@@ -1,48 +1,28 @@
 <template>
     <div class="p-2 ">
-        <PageHeader :button="false" button-text="" title="发布实验" />
-        <Loading v-if="!roomsList" />
+        <PageHeader :button="false" button-text="" title="发布试题" />
+        <Loading v-if="!experimentsList" />
         <div v-else class="grid mt-10 px-20 grid-cols-1 mx-auto gap-x-8 gap-y-8 md:grid-cols-3">
             <div class="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-3">
                 <div class="px-4 py-6 sm:p-8">
                     <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-7">
-                        <div class="sm:col-span-3">
+                        <div class="sm:col-span-2">
                             <label for="website" class="block text-sm font-medium leading-6 text-gray-900">实验名称</label>
                             <div class="mt-2">
-                                <div
-                                    class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-sm">
-                                    <input type="text" name="website" id="website" v-model="experimentName"
-                                        class="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                        placeholder="请填写实验名称" />
-                                </div>
-                            </div>
-                        </div>
-                        <div class="sm:col-span-2">
-                            <label for="website" class="block text-sm font-medium leading-6 text-gray-900">实验教室</label>
-                            <div class="mt-2">
-                                <select class="border py-1 px-6 rounded-md outline-none" v-model="roomsName">
-                                    <option value="" disabled selected>请选择上课教室</option>
-                                    <option v-for="item in roomsList" :value="item.r_id"
-                                        :label="item.r_build + ' ' + item.r_no" />
-                                </select>
-                            </div>
-                        </div>
-                        <div class="sm:col-span-1">
-                            <label for="website" class="block text-sm font-medium leading-6 text-gray-900">实验时间</label>
-                            <div class="mt-2">
-                                <select class="border py-1 px-6 rounded-md outline-none" v-model="experimentTime">
-                                    <option value="" disabled selected>请选择实验时间</option>
-                                    <option v-for="item in experimentTimeList" :value="item" :label="item" />
+                                <select class="border py-1 px-6 rounded-md outline-none" v-model="experimentName">
+                                    <option value="" disabled selected>请选择实验</option>
+                                    <option v-for="item in experimentsList" :value="item.e_id"
+                                        :label="item.e_name" />
                                 </select>
                             </div>
                         </div>
                         <div class="col-span-full">
-                            <label for="about" class="block text-sm font-medium leading-6 text-gray-900">实验简介</label>
+                            <label for="about" class="block text-sm font-medium leading-6 text-gray-900">试题内容</label>
                             <div class="mt-2">
-                                <textarea id="about" name="about" rows="3" v-model="experimentContent"
+                                <textarea id="about" name="about" rows="3" v-model="questionsContent"
                                     class="block w-full px-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                             </div>
-                            <p class="mt-3 text-sm leading-6 text-gray-600">描述一下本次实验吧</p>
+                            <p class="mt-3 text-sm leading-6 text-gray-600">描述一下吧</p>
                         </div>
                         <div class="col-span-full">
                             <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900">上传文件</label>
@@ -75,40 +55,32 @@
 </template>
 
 <script lang="ts" setup>
-import SectionHeader from '../../../../components/SectionHeader.vue';
 import PageHeader from '../../../../components/PageHeader.vue';
-import { selectAllRooms } from '../../../../api/public';
-import { releaseExperiment } from '../../../../api/teachers';
+import { selectAllExperiments } from '../../../../api/public';
+import { releaseQuestion } from '../../../../api/teachers';
 import { ElMessage } from 'element-plus';
 import Loading from '../../../../components/Loading.vue';
 import { ref, onMounted } from 'vue';
-const roomsList = ref();
-const roomsName = ref('');
+const experimentsList = ref();
 const experimentName = ref('');
-const experimentTime = ref('');
-const experimentContent = ref('');
-const experimentTimeList = ['8:30-10:30', '10:50-12:10', '14:00-15:30', '15:40-17:30', '19:00-20:30']
+const questionsContent = ref('');
 const refetch = () => {
-    selectAllRooms().then(res => {
-        roomsList.value = res.data.data;
+    selectAllExperiments().then(res => {
+        experimentsList.value = res.data.data;
     })
 }
 const handleRelease = () => {
-    const e_name = experimentName.value;
-    const e_content = experimentContent.value;
-    const r_id = roomsName.value;
-    const e_time = experimentTime.value;
+    const e_id = experimentName.value;
+    const q_content = questionsContent.value;
     const user = JSON.parse(sessionStorage.getItem('user') as string);
-    console.log(e_name, e_content, r_id, e_time);
-
-    if (!(e_name && e_content && r_id && e_time)) {
+    if (!(e_id && q_content)) {
         ElMessage({
-            message: '请检查实验名、教室、实验简介是否填写完成',
+            message: '请检查试题内容、实验名称是否填写完成',
             type: 'error',
         })
         return;
     }
-    releaseExperiment({ e_name, e_content, r_id, e_time, t_no: user.t_no }).then(res => {
+    releaseQuestion({ q_content, e_id, t_no: user.t_no }).then(res => {
         const { msg, data, status } = res.data;
         if (status) {
             ElMessage({
@@ -116,19 +88,15 @@ const handleRelease = () => {
                 type: 'success',
             })
             experimentName.value = '';
-            experimentContent.value = '';
-            roomsName.value = '';
-            experimentTime.value = '';
-            refetch();
+            questionsContent.value = '';
         } else {
             ElMessage({
                 message: msg,
                 type: 'error',
             })
             experimentName.value = '';
-            experimentContent.value = '';
-            roomsName.value = '';
-            experimentTime.value = '';
+            experimentName.value = '';
+            questionsContent.value = '';
         }
     })
 }
